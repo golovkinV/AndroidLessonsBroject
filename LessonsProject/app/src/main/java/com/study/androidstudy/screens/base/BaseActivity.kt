1f609bccc83e834d05c1262529e9571d64ce1c77
Vladimir Golovkin <vladimir.golovkin16@yandex.ru>
2020-11-15 02:59:16 +0500

    add: math calculation layout

--- LessonsProject/app/src/main/java/com/study/androidstudy/screens/base/BaseActivity.kt
+++ LessonsProject/app/src/main/java/com/study/androidstudy/screens/base/BaseActivity.kt
@@ -13,9 +13,7 @@ abstract class BaseActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         setContentView(layoutResource)
-
         setupViews()
         setupListeners()
     }
---
+++ LessonsProject/app/src/main/java/com/study/androidstudy/screens/math_calculation/MathCalculation.kt
@@ -0,0 +1,4 @@
+package com.study.androidstudy.screens.math_calculation
+
+class MathCalculation {
+}
